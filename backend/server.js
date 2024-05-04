e1f6c7e5011d64c87be8b707199023b20d99df00
unknown <iasafianu@st.ug.edu.gh>
2024-05-04 15:26:43 +0000

    after getting the mongo atlass to work

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,20 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
-import path from "path";
 import taskRouter from "./routes/taskRoutes.js";
+
+import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+app.use(express.static(path.join(__dirname, "dist")));
 
 app.use("/api/task", taskRouter);
 
